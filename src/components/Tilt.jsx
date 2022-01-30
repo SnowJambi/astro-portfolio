9b84b2095b54da8b6bb912f249cb18f4186d0fb0
Lachlan Jadezak <Lachlan@Jadezak.com>
2022-01-30 17:54:00 +1100

    Mobile optimisation. Test globall scaling text size

--- src/components/Tilt.jsx
+++ src/components/Tilt.jsx
@@ -2,7 +2,7 @@ import Tilt from 'react-parallax-tilt'
 
 const TiltObject = ( props ) => {
   return (
-        <Tilt className='project' tiltReverse={true} perspective={2000} glareEnable={true} glareMaxOpacity={0.45} scale={1.15}>
+        <Tilt className='project' tiltReverse={true} perspective={2000} glareEnable={true} glareMaxOpacity={0.45} scale={1.05} glareBorderRadius={'10px'}>
           {props.children}
         </Tilt>
   )
